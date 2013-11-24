18506c8f67aa564468751cd1679aefd9e6aecc43
SEBA <scanevaro@hotmail.com>
2013-11-23 21:45:43 -0300

    movement fix

--- DuckHuntPrototipe/src/com/deeep/duckhuntprototipe/entities/Duck.java
+++ DuckHuntPrototipe/src/com/deeep/duckhuntprototipe/entities/Duck.java
@@ -10,7 +10,7 @@ public class Duck extends DynamicGameObject {
 	public static final int DUCK_STATE_FLYING = 0;
 	public static final int DUCK_STATE_HIT = 1;
 	public static final int DUCK_STATE_FALLING = 2;
-	public static final float DUCK_VELOCITY = 10;
+	public static final float DUCK_VELOCITY = 5;
 	public static final float DUCK_GRAVITY = -0.5f;
 	public static final float DUCK_WIDTH = 1.25f;
 	public static final float DUCK_HEIGHT = 1.25f;
@@ -22,18 +22,25 @@ public class Duck extends DynamicGameObject {
 	public Duck(float x, float y) {
 		super(x, y, DUCK_WIDTH, DUCK_HEIGHT);
 		state = DUCK_STATE_FLYING;
-		velocity.set(DUCK_VELOCITY, 10);
+		velocity.set(DUCK_VELOCITY, DUCK_VELOCITY);
 		stateTime = 0;
 		rand = new Random();
 	}
 
 	public void update(float deltaTime) {
-		
+
 		switch (state) {
 		case DUCK_STATE_FLYING:
 			position.add(velocity.x * deltaTime, velocity.y * deltaTime);
 			bounds.x = position.x - DUCK_WIDTH / 2;
 			bounds.y = position.y - DUCK_HEIGHT / 2;
+
+			if (rand.nextFloat() > 0.999f) {
+				velocity.x = -velocity.x;
+			}
+			if (rand.nextFloat() > 0.999f) {
+				velocity.y = -velocity.y;
+			}
 
 			if (position.x < DUCK_WIDTH / 2) {
 				position.x = DUCK_WIDTH / 2;
@@ -61,7 +68,7 @@ public class Duck extends DynamicGameObject {
 			}
 			break;
 		case DUCK_STATE_HIT:
-			if (stateTime > 1.0f){
+			if (stateTime > 1.0f) {
 				state = DUCK_STATE_FALLING;
 				velocity.set(0, DUCK_GRAVITY);
 			}
@@ -73,7 +80,7 @@ public class Duck extends DynamicGameObject {
 			bounds.y = position.y - DUCK_HEIGHT / 2;
 			break;
 		}
-		
+
 		stateTime += deltaTime;
 	}
 
